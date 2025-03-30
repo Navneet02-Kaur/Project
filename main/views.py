b69d003cf43dce7affb624f049696ba554dd0fec
Navneet02-Kaur <navneetkaurbiji@gmail.com>
2025-03-30 11:50:15 +0530

    fewchanges

--- main/views.py
+++ main/views.py
@@ -126,30 +126,30 @@ def offset(request):
 def tips(request):
     return render(request, 'main/tips.html')
 
-def user_list(request):
-    users = Users.objects.all()
-    return render(request, 'main/user_list.html', {'users': users})
+# def user_list(request):
+#     users = Users.objects.all()
+#     return render(request, 'main/user_list.html', {'users': users})
 
-def user_create(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('user_list')
-    else:
-        form = UserForm()
-    return render(request, 'main/user_form.html', {'form': form})
+# def user_create(request):
+#     if request.method == 'POST':
+#         form = UserForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('user_list')
+#     else:
+#         form = UserForm()
+#     return render(request, 'main/user_form.html', {'form': form})
 
-def user_update(request, id):
-    user = Users.objects.get(id=id)
-    if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            return redirect('user_list')
-    else:
-        form = UserForm(instance=user)
-    return render(request, 'main/user_form.html', {'form': form})
+# def user_update(request, id):
+#     user = Users.objects.get(id=id)
+#     if request.method == 'POST':
+#         form = UserForm(request.POST, instance=user)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('user_list')
+#     else:
+#         form = UserForm(instance=user)
+#     return render(request, 'main/user_form.html', {'form': form})
 
 
 def tips(request):
